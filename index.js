654ead71ee82bc23889fb8c73f3ec50cc081ae17
Hikmet Meskic <hikmet.meskic2018@size.ba>
2021-12-30 17:40:45 +0100

    Add subreddit template

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const redditData = require('./data.json');
+// Set EJS path to ./views
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
+    res.render('subreddit', { subreddit });
+})
 
 app.listen(8080, () => {
     console.log("LISTENING ON PORT 8080")
