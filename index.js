0a23a9b3a1ee71fd2619e2dd4d45b32fa156d83e
Hikmet Meskic <hikmet.meskic2018@size.ba>
2021-12-30 17:29:33 +0100

    Add EJS for templating

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
 
 app.listen(8080, () => {
-    console.log("LISTENING ON PORT 3000")
+    console.log("LISTENING ON PORT 8080")
 })
